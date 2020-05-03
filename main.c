b06773ab3ba0e52b75025f367dc8ac9c5f5f16ce
tuankietkaito <tuankietkaito@gmail.com>
2020-05-03 22:18:18 +0700

    ew

--- main.c
+++ main.c
@@ -5,15 +5,20 @@ int main()
 {
     printf("Block A\n");
     int *pA = (int *)aligned_malloc(12, 5);
+    printf("\n");
     printf("Block B\n");
     int *pB = (int *)aligned_malloc(12, 4);
+    printf("\n");
     printf("Block C\n");
     int *pC = (int *)aligned_malloc(12, 4);
+    printf("\n"); 
 
     printf("Free Block A\n");
     aligned_free(pB);
+    printf("\n");
     printf("Free Block B\n");
     aligned_free(pA);
+    printf("\n");
     printf("Free Block C\n");
     aligned_free(pC);
 }
