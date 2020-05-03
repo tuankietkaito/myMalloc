71cabf86ad6425f1d5d33b0f1d6372b320bda86d
tuankietkaito <tuankietkaito@gmail.com>
2020-05-03 22:15:42 +0700

    sfev

--- main.c
+++ main.c
@@ -3,11 +3,17 @@
 
 int main()
 {
+    printf("Block A\n");
     int *pA = (int *)aligned_malloc(12, 5);
+    printf("Block B\n");
     int *pB = (int *)aligned_malloc(12, 4);
+    printf("Block C\n");
     int *pC = (int *)aligned_malloc(12, 4);
 
+    printf("Free Block A\n");
     aligned_free(pB);
+    printf("Free Block B\n");
     aligned_free(pA);
+    printf("Free Block C\n");
     aligned_free(pC);
 }
